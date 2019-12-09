c51a843d3185978867bca60c73ce79d358ac83e6
asuthamm <asuthamm@gmail.com>
2019-12-09 13:17:30 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
+# app.rb
 require_relative 'config/environment'
 
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Started my server using Shotgun!"
+  end
+
+  get '/new' do 
+    "Welcome to your app!!!! I BUILT THIS....!"
   end
 
 end
